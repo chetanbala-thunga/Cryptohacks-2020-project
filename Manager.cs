1b60a117ae60afe066851ce4a992e4aec9d98da6
chetanbala-thunga <40259259+chetanbala-thunga@users.noreply.github.com>
2020-02-16 08:07:28 -0800

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float delay = 1f;
+
+    public GameObject CompleteLevelUI;
+    
+    public void CompleteLevel ()
+	{
+		CompleteLevelUI.SetActive(true);
+	}
+    
+    
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", delay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public PlayerMovement movement;
+
+    public float fForce = 2000f;
+    public float rForce = 500f;
+    public float lForce = -500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,fForce * Time.deltaTime);
+
+        if (Input.GetKey("d")){
+            rb.AddForce(rForce * Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+        
+        if (Input.GetKey("a")){
+            rb.AddForce(lForce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<Manager>().EndGame();
+        }
+    }
+}
